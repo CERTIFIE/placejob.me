9946443438997285ec9138831cb1892bd135e324
Lorenzo Colautti <colautti.lorenzo@certifie.me>
2025-07-29 15:32:15 +0200

    editing tiers

--- src/data/pricing.ts
+++ src/data/pricing.ts
@@ -2,36 +2,105 @@ import { IPricing } from "@/types";
 
 export const tiers: IPricing[] = [
     {
-        name: 'Starter',
-        price: 29,
+        name: 'User',
+        type: 'user',
+        price: {
+            always: {
+                period: 'per sempre',
+                price: 'Gratuito',
+            }
+        },
         features: [
-            'Basic cloud integration',
-            'Up to 5 team members',
-            '20GB storage',
-            'Email support',
+            'IA integrata per creazione profilo e ricerca lavoro',
+            'Social network per connessioni professionali',
+            'Creazione automatico del CV con template Europass',
+            'Accesso a corsi di formazione',
+            'Certificazione tramite blockchain/NFT',
+            'Supporto via email o tramite app'
         ],
     },
     {
-        name: 'Pro',
-        price: 99,
+        name: 'HR manager',
+        type: 'corporate',
+        price: {
+            month: {
+                period: 'mese',
+                price: 29.59,
+            },
+            year: {
+                period: 'anno',
+                price: 299.00,
+            }
+        },
         features: [
-            'Advanced cloud integration',
-            'Up to 20 team members',
-            '100GB storage',
-            'Priority email & phone support',
-            'Advanced analytics',
+            'IA integrata per ricerca candidati',
+            'Fino a 10 aziende in selezione',
+            'Messaggistica diretta con candidati',
+            'Stampa dei CV in PDF Europass dei candidati',
+            'Reportistica avanzata',
+            'Supporto via email o tramite app',
+        ],
+    },
+    {
+        name: 'Courses',
+        type: 'corporate',
+        price: {
+            month: {
+                period: 'mese',
+                price: 99.59,
+            },
+            year: {
+                period: 'anno',
+                price: 999.00,
+            }
+        },
+        features: [
+            'Integrazione con sistemi esistenti',
+            'Creazione corsi di formazione',
+            'Certificazione tramite blockchain/NFT',
+            'Accesso a reportistica avanzata',
+            'Supporto dedicato 24/7',
+        ],
+    },
+    {
+        name: 'HR Organization',
+        type: 'corporate',
+        price: {
+            month: {
+                period: 'mese',
+                price: 99.59,
+            },
+            year: {
+                period: 'anno',
+                price: 999.00,
+            }
+        },
+        features: [
+            'IA integrata per ricerca candidati',
+            'Fino a 10 posizioni aperte in contemporanea',
+            'Messaggistica diretta con candidati',
+            'Stampa dei CV in PDF Europass dei candidati',
+            'Reportistica avanzata',
+            'Supporto dedicato 24/7',
         ],
     },
     {
         name: 'Enterprise',
-        price: 'Custom',
+        type: 'corporate',
+        price: {
+            custom: {
+                period: 'year',
+                price: 'custom',
+            }
+        },
         features: [
-            'Full cloud integration',
-            'Unlimited team members',
-            'Unlimited storage',
-            '24/7 dedicated support',
-            'Custom solutions',
-            'On-site training',
+            'IA integrata per ricerca candidati',
+            'Fino a 10 posizioni aperte in contemporanea',
+            'Messaggistica diretta con candidati',
+            'Stampa dei CV in PDF Europass dei candidati',
+            'Reportistica avanzata',
+            'Supporto dedicato 24/7',
+            'Integrazione con sistemi HR esistenti',
         ],
     },
 ]
